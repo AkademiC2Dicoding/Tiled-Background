70b71f01f375961fad9369907345be3b7b071f90
Unknown <punto.damar2@gmail.com>
2019-02-15 13:22:51 +0700

    export game

---
+++ docs/offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1550211735,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bg.png",
+		"images/heartempty.png",
+		"images/heartfull.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
